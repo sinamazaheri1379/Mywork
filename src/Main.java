24277a8ef9035388d57f3dee0a719f3d7a2ab173
sinamazaheri1379 <sinamazaheri1707@gmail.com>
2020-03-15 18:51:04 +0330

    add getting string for input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello");
+        Scanner scanner =new Scanner(System.in);
+        String input=scanner.nextLine();
+        System.out.println(input);
     }
 }
