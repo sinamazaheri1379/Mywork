73a9aaa710f10fde3c5efcbaaaf8b5e57b83bd40
sinamazaheri1379 <sinamazaheri1707@gmail.com>
2020-03-15 19:53:10 +0330

    change String to int

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner =new Scanner(System.in);
-        String input=scanner.nextLine();
+        int input=scanner.nextInt();
         System.out.println(input);
     }
 }
